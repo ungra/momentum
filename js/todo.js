150bc8ad71cbd5ae6d3e1733cc7bddf64c9eb2f0
junhee_lee2 <junhee_lee2@tmax.co.kr>
2023-09-01 09:21:36 +0900

    Deleting Todos

--- js/todo.js
+++ js/todo.js
@@ -2,13 +2,21 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = document.querySelector("#todo-form input");
 const todoList = document.getElementById("todo-list");
 
+function deleteTodo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "X";
+  button.addEventListener("click", deleteTodo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
-  console.log(li);
 }
 
 function handleToDoSubmit(event) {
